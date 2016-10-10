092b9c18acf9e68a35525d96c60b6789c039e203
sylviahubbard1 <sylviahubbard@gmail.com>
2016-10-10 11:51:04 -0400

    update now w/noah

---
+++ Lab28YouTube/Controllers/YouTubeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab28YouTube.Models;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace Lab28YouTube.Controllers
+{
+    public class YouTubeController : Controller
+    {
+        // GET: YouTube
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Index(YouTubeModel model)
+        {
+            await PerformYouTubeSearchAsync(model);
+            return View("SearchResults", model);
+        }
+
+        private async Task PerformYouTubeSearchAsync(YouTubeModel model)
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyCTnrb4UFBFj3NknsqAKem6Chf1fbLdhT0",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = model.Keywords; // Replace with your search term.
+            searchListRequest.MaxResults = 10;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        model.Videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+                        break;
+
+                    case "youtube#channel":
+                        model.Channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
+                        break;
+
+                    case "youtube#playlist":
+                        model.Playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Lab28YouTube/Models/YouTubeModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab28YouTube.Models
+{
+    public class YouTubeModel
+    {
+        public string Keywords
+        {
+            get;set;
+        }
+        public List<string> Videos { get; set; }
+        public List<string> Channels { get; set; }
+        public List<string> Playlists { get; set; }
+
+        public YouTubeModel()
+        {
+            Videos = new List<string>();
+            Channels = new List<string>();
+            Playlists = new List<string>();
+        }
+    }
+}
